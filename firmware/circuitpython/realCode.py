7d9fb77eaa5efdb2c1484c72d0d9554e6ae67767
Jerick Seng <64419693+jer123se12@users.noreply.github.com>
2025-06-09 00:15:53 +0800

    Update realCode.py

--- firmware/circuitpython/realCode.py
+++ firmware/circuitpython/realCode.py
@@ -5,35 +5,40 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 import digitalio
 
-# get the pins
+# Initialize the pin
 pin1 = digitalio.DigitalInOut(board.GP27)
+# Set direction and pull
+pin1.direction = digitalio.Direction.INPUT
+pin1.pull = digitalio.Pull.UP
+# Add the debouncer
+switch1 = Debouncer(pin1)
+
+
+# do the rest of the pins
 pin2 = digitalio.DigitalInOut(board.GP26)
 pin3 = digitalio.DigitalInOut(board.GP15)
-
-# set their setting
-pin1.direction = digitalio.Direction.INPUT
 pin2.direction = digitalio.Direction.INPUT
 pin3.direction = digitalio.Direction.INPUT
-pin1.pull = digitalio.Pull.UP
 pin2.pull = digitalio.Pull.UP
 pin3.pull = digitalio.Pull.UP
 
-# add debouncer
-switch1 = Debouncer(pin1)
 switch2 = Debouncer(pin2)
 switch3 = Debouncer(pin3)
 
+# Initailize the keyboard
 kbd = Keyboard(usb_hid.devices)
 
 while True:
     # Update Switches
     switch1.update()
-    switch2.update()
-    switch3.update()
+    
 
     # Check if switch is pressed and update accordingly
     if (switch1.rose):
         kbd.send(Keycode.LEFT_CONTROL, Keycode.C)
+    
+    switch2.update()
+    switch3.update()
     if (switch2.rose):
         kbd.send(Keycode.LEFT_CONTROL, Keycode.V)
     if (switch3.rose):
